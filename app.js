f9e715d5b6ccb3bc0b9f99cbe0b25103cd9e99d6
taveershah <tanvirhussein@gmail.com>
2020-08-13 15:17:45 -0400

    page and user models added

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-const express = require('express');
-const morgan = require('morgan');
-const htmltag = require('html-template-tag');
+const express = require("express");
+const morgan = require("morgan");
+const { db } = require("./models");
 
 const app = express();
 
-app.use(morgan('dev'));
-app.use(express.static(__dirname + '/public'));
-app.use(express.urlencoded({extended: false}));
+db.authenticate().then(() => {
+  console.log("connected to the database");
+});
 
-app.get('/', (req, res, next) => {
-  res.send('hello world');
-})
+app.use(morgan("dev"));
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: false }));
 
-const PORT = 3000
-app.listen(PORT,() => {console.log(`app is running on port: ${PORT}`)});
+app.get("/", (req, res, next) => {
+  res.send("hello world");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`app is running on port: ${PORT}`);
+});
